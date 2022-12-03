bf284dd3bdbff43bb427d63dafab2203673f6ebc
Maximilian Mauroner <github@mauroner.eu>
2022-12-03 10:23:22 +0100

    Day 3 done was way easier than day 2

---
+++ src/twentytwentytwo/Three.java
@@ -0,0 +1,62 @@
+package twentytwentytwo;
+
+import java.util.Scanner;
+
+public class Three {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println();
+        int sum = 0;
+        while (scanner.hasNextLine()) {
+            System.out.println();
+            String s = scanner.nextLine();
+            String left;
+            String right;
+            left = s.substring(0, s.length() / 2);
+            right = s.substring(s.length() / 2);
+            String checked = "";
+            for (int i = 0; i < left.length(); i++) {
+                if (right.contains(left.charAt(i) + "") && !checked.contains(left.charAt(i) + "")) {
+                    int temp = 0;
+                    checked += left.charAt(i) + "";
+                    if (left.charAt(i) > 96) {
+                        temp = (int) (left.charAt(i) - 96);
+                    } else {
+                        temp = (int) (left.charAt(i) - 65 + 27);
+                    }
+                    sum += temp;
+                }
+            }
+            System.out.println(sum);
+        }
+    }
+}
+
+class ThreeB {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println();
+        int sum = 0;
+        while (scanner.hasNextLine()) {
+            System.out.println();
+            String[] s = {scanner.nextLine(), scanner.nextLine(), scanner.nextLine()};
+            String checked = "";
+            for (int i = 0; i < s[0].length(); i++) {
+                if (s[1].contains(s[0].charAt(i) + "") && s[2].contains(s[0].charAt(i) + "")) {
+                    int temp = 0;
+                    checked += s[0].charAt(i) + "";
+                    if (s[0].charAt(i) > 96) {
+                        temp = (int) (s[0].charAt(i) - 96);
+                    } else {
+                        temp = (int) (s[0].charAt(i) - 65 + 27);
+                    }
+                    sum += temp;
+                    break;
+                }
+            }
+            System.out.println(sum);
+        }
+    }
+}
